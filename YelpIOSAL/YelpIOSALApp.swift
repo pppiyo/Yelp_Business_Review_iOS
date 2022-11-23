0f9f9c6b37777ad804fe714053b10116461dee86
pppiyo <yueqinli@usc.edu>
2022-11-23 11:36:51 -0800

    Initial Commit

---
+++ YelpIOSAL/YelpIOSALApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpIOSALApp.swift
+//  YelpIOSAL
+//
+//  Created by Amy Lee on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct YelpIOSALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
